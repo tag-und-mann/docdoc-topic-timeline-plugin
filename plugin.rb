aaf135d934126dfedbf5de452449848611abb1b0
Vladyslav Sydorenko <svuitis@gmail.com>
2020-02-25 12:18:00 +0200

    move topic-view plugin, add feed fetch

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,12 @@
 # name: berta-topic-timeline-plugin
 # about: Changes for standard topic view
-# version: 0.0.2
-# authors: Vadim Bondar
+# version: 0.1.0
+# authors: Vadim Bondar, Vladyslav Sydorenko
 
 enabled_site_setting :timeline_sidebar_enabled
 
+register_asset 'stylesheets/topics_common.scss'
+register_asset 'stylesheets/topics_mobile.scss'
 register_asset 'slick/css/slick.scss'
 register_asset 'stylesheets/timeline_sidebar_common.scss'
 register_asset 'stylesheets/timeline_topics_common.scss'
@@ -15,7 +17,88 @@ register_asset 'slick/slick.min.js'
 
 after_initialize do
   add_to_class :post, :excerpt_for_topic do
-    Post.excerpt(cooked, 200, strip_links: true)
+    Post.excerpt(cooked, 300, strip_links: true, strip_images: false, post: self)
   end
   add_to_serializer(:listable_topic, :include_excerpt?) { true }
+
+  require_dependency "application_controller"
+  require_dependency "plugin_store"
+  require_dependency 'rss'
+
+  module ::TopicTimeline
+    PLUGIN_NAME = 'topic_timeline'.freeze
+
+    class Engine < ::Rails::Engine
+      engine_name TopicTimeline::PLUGIN_NAME
+      isolate_namespace TopicTimeline
+    end
+  end
+
+  class TopicTimeline::ReceiveFeed
+    attr_reader :uri
+
+    def initialize(uri)
+      @uri = uri
+    end
+
+    def call
+      rss = RSS::Parser.parse(uri)
+
+      channel = channel_data(rss.channel)
+      items = items_data(rss.items)
+
+      { channel_data: channel, items: items }
+    rescue => e
+      { error: e.to_s }
+    end
+
+    private
+
+    def channel_data(channel)
+      {
+        title: channel.title,
+        link: channel.link,
+        description: channel.description,
+        copyright: channel.copyright,
+        language: channel.language,
+        image: {
+          url: channel.image.url,
+          title: channel.image.title,
+          link: channel.image.link
+        }
+      }
+    end
+
+    def items_data(items)
+      items.map do |item|
+        {
+          title: item.title,
+          author: item.author,
+          description: item.description
+        }
+      end
+    end
+  end
+
+  class TopicTimeline::TopicTimelineController < ::ApplicationController
+    requires_plugin TopicTimeline::PLUGIN_NAME
+
+    before_action :ensure_logged_in
+    skip_before_action :check_xhr
+
+    def feed
+      feed_data = TopicTimeline::ReceiveFeed.new(params[:uri]).call
+      status = feed_data[:error] ? 422 : 200
+      render json: feed_data, status: status
+    end
+  end
+
+
+  TopicTimeline::Engine.routes.draw do
+    get '/' => 'topic_timeline#feed'
+  end
+
+  Discourse::Application.routes.append do
+    mount ::TopicTimeline::Engine, at: '/rss_feed'
+  end
 end
